9a1e052754b035ec18973d61033a0f2d823fba85
benoitader <benoitader.dev@gmail.com>
2022-03-14 23:28:29 +0100

    Redux Toolkit et développement d'API ✅

--- project/src/components/Home.jsx
+++ project/src/components/Home.jsx
@@ -3,18 +3,25 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
 const { Title } = Typography;
 
 const Home = () => {
+    const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+
+    if(isFetching) return 'Loading ...';
+
     return (
       <>
         <Title level={2} className="heading">Statistiques mondiales sur les crypto-monnaies</Title>
         <Row>
-          <Col span={12}><Statistic title="Total des crypto-monnaies" value="5"/></Col>
-          <Col span={12}><Statistic title="Total des échanges" value="5"/></Col>
-          <Col span={12}><Statistic title="Capitalisation boursière totale" value="5"/></Col>
-          <Col span={12}><Statistic title="Volume total sur 24h" value="5"/></Col>
-          <Col span={12}><Statistic title="Total des marchés" value="5"/></Col>
+          <Col span={12}><Statistic title="Total des crypto-monnaies" value={globalStats.total}/></Col>
+          <Col span={12}><Statistic title="Total des échanges" value={millify(globalStats.totalExchanges)}/></Col>
+          <Col span={12}><Statistic title="Capitalisation boursière totale" value={millify(globalStats.totalMarketCap)}/></Col>
+          <Col span={12}><Statistic title="Volume total sur 24h" value={millify(globalStats.total24hVolume)}/></Col>
+          <Col span={12}><Statistic title="Total des marchés" value={millify(globalStats.totalMarkets)}/></Col>
         </Row>
       </>
     )
