864077a027250cbab237462622dea1a0a38fd0a4
benoitader <benoitader.dev@gmail.com>
2022-03-16 13:50:38 +0100

    Responsive Menu✅

--- project/src/components/Navbar.jsx
+++ project/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
@@ -6,6 +6,28 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/cryptoview.png';
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+
+  }, []);
+
+  useEffect(() => {
+    if(screenSize < 768) {
+      setActiveMenu(false)
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize])
+
     return (
         <div className="nav-container">
           <div className="logo-container">
@@ -13,26 +35,31 @@ const Navbar = () => {
             <Typography.Title level={2} className="logo">
               <Link to="/">Cryptoview</Link>
             </Typography.Title>
+            <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
+              <MenuOutlined />
+            </Button>
           </div>
-          <Menu theme="dark">
+          {activeMenu && (
+            <Menu theme="dark">
 
-            <Menu.Item icon={<HomeOutlined />}>
-              <Link to="/">Accueil</Link>
-            </Menu.Item>
+              <Menu.Item icon={<HomeOutlined />}>
+                <Link to="/">Accueil</Link>
+              </Menu.Item>
 
-            <Menu.Item icon={<FundOutlined />}>
-              <Link to="/cryptomonnaies">Cryptomonnaies</Link>
-            </Menu.Item>
+              <Menu.Item icon={<FundOutlined />}>
+                <Link to="/cryptomonnaies">Cryptomonnaies</Link>
+              </Menu.Item>
 
-            <Menu.Item icon={<MoneyCollectOutlined />}>
-              <Link to="/echanges">Échanges</Link>
-            </Menu.Item>
+              <Menu.Item icon={<MoneyCollectOutlined />}>
+                <Link to="/echanges">Échanges</Link>
+              </Menu.Item>
 
-            <Menu.Item icon={<BulbOutlined />}>
-              <Link to="/actualites">Actualités</Link>
-            </Menu.Item>
+              <Menu.Item icon={<BulbOutlined />}>
+                <Link to="/actualites">Actualités</Link>
+              </Menu.Item>
 
-          </Menu>
+            </Menu>
+          )}
         </div>
     )
 }
